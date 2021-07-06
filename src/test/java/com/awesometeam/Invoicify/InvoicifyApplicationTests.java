d8f67ea1e14902519aea4bc025c09545cb28d211
EdgarBarrientos <Edgar_O_Barrientos@outlook.com>
2021-07-06 19:53:23 -0400

    initial commit

---
+++ src/test/java/com/awesometeam/Invoicify/InvoicifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesometeam.Invoicify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

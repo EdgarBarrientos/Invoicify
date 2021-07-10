0d1dcdd1581a283c6e28af4292bd6ef07f49346a
Segun Oladapo <segun.holadapo@gmail.com>
2021-07-10 14:46:51 -0500

    As a user, I can add line items to invoices

--- src/test/java/com/awesometeam/Invoicify/invoice/integration/InvoiceDetailsIntegrationTests.java
+++ src/test/java/com/awesometeam/Invoicify/invoice/integration/InvoiceDetailsIntegrationTests.java
@@ -1,40 +1,44 @@
-package com.awesometeam.Invoicify.unit;
+package com.awesometeam.Invoicify.invoice.integration;
+
 
 import com.awesometeam.Invoicify.invoice.model.InvoiceDetails;
 import com.awesometeam.Invoicify.invoice.model.Items;
-import com.awesometeam.Invoicify.invoice.service.InvoiceDetailsService;
+import com.awesometeam.Invoicify.invoice.repository.InvoiceDetailsRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.awesometeam.Invoicify.invoice.utility.Helper.getJSON;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest
-public class InvoiceDetailsAPITests
-{
+import  com.awesometeam.Invoicify.invoice.model.*;
+import  com.awesometeam.Invoicify.invoice.controller.InvoiceController;
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+public class InvoiceDetailsIntegrationTests {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
-    InvoiceDetailsService service;
+    InvoiceDetailsRepository repo;
 
     @Test
-    void addNewLineItemsTest() throws Exception
-    {
+    void addNewLineItemTest() throws Exception {
         List<Items> itemsList = new ArrayList<>();
         itemsList.add (new Items(1L,"item1",'F',0,0.0,20.0));
         itemsList.add (new Items(2L,"item2",'R',10,5.0,0.0));
@@ -47,19 +51,10 @@ public class InvoiceDetailsAPITests
             InvoiceDetails invoiceDetails1 = invocation.getArgument(0);
             invoiceDetails1.setInvoiceId(1L);
             return null;
-        }).when(service).addNewLineItem(isA(InvoiceDetails.class));
-
-        //when(service.addNewLineItem(invoiceDetails)).thenReturn(invoiceDetails);
+        }).when(repo).save(isA(InvoiceDetails.class));
         this.mvc.perform(post("/addInvoiceItem")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
-                .andExpect(status().isOk());
-
-
+                .andExpect(status().isCreated());
     }
-    public String getJSON(String path) throws Exception {
-        Path paths = Paths.get(path);
-        return new String(Files.readAllBytes(paths));
-    }
-
 }
